c338d0fd3121a31b9c268af3d2ec7b655a5966ee
synthiaatieno <ondengsynthia222@gmail.com>
2022-04-14 18:53:48 +0300

    first commit

---
+++ ArrayCode.java
@@ -0,0 +1,19 @@
+public class ArrayCode{
+    public staic void main(String[]args)
+    {
+        int []arr = new int[5];
+        arr[0]=6;
+        arr[1]=7;
+        arr[2]=3;
+        arr[3]=9;
+        arr[4]=2;
+
+        int sum=0;
+
+        for(int i=0; i<arr.length; i++)
+        {
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+}
